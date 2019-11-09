dc77634fee43083fc7df8f83a5d5bfeeaba53d6a
GabrielBorglund <borglund.g@gmail.com>
2019-11-09 12:00:46 +0100

    Updates

---
+++ precache-manifest.d15566b087674e8a6fb458a3e9508243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585fc148524d1691b05325c6e72dbb12",
+    "url": "/cremisweb/index.html"
+  },
+  {
+    "revision": "d7102a016630300c6b09",
+    "url": "/cremisweb/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9d56e5c7a398f1c784b9",
+    "url": "/cremisweb/static/js/2.84dc6be4.chunk.js"
+  },
+  {
+    "revision": "d7102a016630300c6b09",
+    "url": "/cremisweb/static/js/main.321c40f8.chunk.js"
+  },
+  {
+    "revision": "4b1aad46e84a0322d0c0",
+    "url": "/cremisweb/static/js/runtime-main.31e0c826.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cremisweb/static/media/logo.25bf045c.svg"
+  }
+]);
